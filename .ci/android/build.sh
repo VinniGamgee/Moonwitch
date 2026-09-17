66c1e559abbf8beb17b3a301e42546565fa2cff1
VinniGamgee <viniciuscardoso0033@gmail.com>
2026-09-16 22:25:41 -0300

    build: remove Eden branding from Android builder

--- .ci/android/build.sh
+++ .ci/android/build.sh
@@ -1,10 +1,9 @@
 #!/bin/sh -e
 
-# SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
+# SPDX-FileCopyrightText: Copyright 2026 Moonwitch Project
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 NUM_JOBS=$(nproc 2>/dev/null || getconf _NPROCESSORS_ONLN 2>/dev/null || echo 2)
-export CMAKE_BUILD_PARALLEL_LEVEL="${NUM_JOBS}"
 ARTIFACTS_DIR="$PWD/artifacts"
 
 : "${CCACHE:=false}"
@@ -27,11 +26,11 @@ Options:
                           	Valid values are: legacy, optimized, standard, chromeos
                           	Default: standard
     -b, --build-type <TYPE>	Build type (variable: TYPE)
-                          	Valid values are: Release, RelWithDebInfo, Debug
+                          	Build type: Release, RelWithDebInfo, Debug
                           	Default: Debug
-    -n, --nightly           Create a nightly build.
+    -n, --nightly           Create a Moonwitch Nightly build.
 
-Extra arguments are passed to CMake (e.g. -DCMAKE_OPTION_NAME=VALUE)
+Extra arguments are passed to CMake.
 Set the CCACHE variable to "true" to enable build caching.
 The APK and AAB will be output into "artifacts".
 
@@ -47,27 +46,24 @@ die() {
 
 target() {
     [ -z "$1" ] && die "You must specify a valid target."
-
     TARGET="$1"
 }
 
 type() {
     [ -z "$1" ] && die "You must specify a valid type."
-
     TYPE="$1"
 }
 
 while true; do
-	case "$1" in
-		-r|--release) DEVEL=false ;;
-		-t|--target) target "$2"; shift ;;
-		-b|--build-type) type "$2"; shift ;;
+    case "$1" in
+        -r|--release) DEVEL=false ;;
+        -t|--target) target "$2"; shift ;;
+        -b|--build-type) type "$2"; shift ;;
         -n|--nightly) NIGHTLY=true ;;
-		-h|--help) usage ;;
-		*) break ;;
-	esac
-
-	shift
+        -h|--help) usage ;;
+        *) break ;;
+    esac
+    shift
 done
 
 : "${TARGET:=standard}"
@@ -77,26 +73,23 @@ done
 TARGET_LOWER=$(echo "$TARGET" | tr '[:upper:]' '[:lower:]')
 
 case "$TARGET_LOWER" in
-	legacy) FLAVOR=Legacy ;;
-	optimized) FLAVOR=GenshinSpoof ;;
-	standard) FLAVOR=Mainline ;;
+    legacy) FLAVOR=Legacy ;;
+    optimized) FLAVOR=GenshinSpoof ;;
+    standard) FLAVOR=Mainline ;;
     chromeos) FLAVOR=ChromeOS ;;
-	*) die "Invalid build flavor $TARGET."
+    *) die "Invalid build flavor $TARGET." ;;
 esac
 
 case "$TYPE" in
-	RelWithDebInfo|Release|Debug) ;;
-	*) die "Invalid build type $TYPE."
+    RelWithDebInfo|Release|Debug) ;;
+    *) die "Invalid build type $TYPE" ;;
 esac
-
-LOWER_FLAVOR=$(echo "$FLAVOR" | sed 's/./\L&/')
-LOWER_TYPE=$(echo "$TYPE" | sed 's/./\L&/')
 
 if [ -n "${ANDROID_KEYSTORE_B64}" ]; then
     export ANDROID_KEYSTORE_FILE="${GITHUB_WORKSPACE}/ks.jks"
     echo "${ANDROID_KEYSTORE_B64}" | base64 --decode > "${ANDROID_KEYSTORE_FILE}"
-	SHA1SUM=$(keytool -list -v -storepass "${ANDROID_KEYSTORE_PASS}" -keystore "${ANDROID_KEYSTORE_FILE}" | grep SHA1 | cut -d " " -f3)
-	echo "-- Keystore SHA1 is ${SHA1SUM}"
+    SHA1SUM=$(keytool -list -v -storepass "${ANDROID_KEYSTORE_PASS}" -keystore "${ANDROID_KEYSTORE_FILE}" | grep SHA1 | cut -d " " -f3)
+    echo "-- Keystore SHA1 is ${SHA1SUM}"
 fi
 
 cd src/android
@@ -118,7 +111,7 @@ else
     NIGHTLY=false
 fi
 
-echo "-- building..."
+echo "-- building Moonwitch..."
 
 ./gradlew "copy${FLAVOR}${TYPE}Outputs" \
     -Dorg.gradle.caching="${CCACHE}" \
@@ -132,6 +125,5 @@ if [ -n "${ANDROID_KEYSTORE_B64}" ]; then
     rm "${ANDROID_KEYSTORE_FILE}"
 fi
 
-echo "-- Done! APK and AAB artifacts are in ${ARTIFACTS_DIR}"
-
+echo "-- Done! Moonwitch APK and AAB artifacts are in ${ARTIFACTS_DIR}"
 ls -l "${ARTIFACTS_DIR}/"
